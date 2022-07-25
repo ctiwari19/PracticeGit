d88a8f188b53c5d629637751c35400adc7b4641a
Chanchal Tiwari <chanchaltiwari448@gmail.com>
2022-07-25 13:01:07 +0530

    this is my first self committttttttt

---
+++ src/main/java/FirstPracGitbash.java
@@ -0,0 +1,15 @@
+
+public class FirstPracGitbash 
+{
+
+	
+		public static void main(String[] args) 
+		{
+			System.out.println(" we r inn");
+			System.out.println("say good bye");
+			System.out.println(" we r inn");
+			System.out.println("say good bye");
+		}
+	
+}
+
